df8838cb5915b5693532cefa207d81e3c70ade57
tnddang <tnd.dang@gmail.com>
2021-02-13 18:48:29 -0800

    Added app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const isbnInput = document.querySelector("#isbn")
+const button = document.querySelector(".btn");
+const bookList = document.querySelector("#book-list");
+
+button.addEventListener("click", function() {
+    if (titleInput.value == "" &&
+        authorInput.value == "" &&
+        isbnInput.value == "") {
+      alert("Please enter a field.");
+    } else {
+      const bookListRow = document.createElement("tr");
+
+      const newTitle = document.createElement("th");
+      newTitle.innerHTML = titleInput.value;
+      bookListRow.appendChild(newTitle);
+
+      const newAuthor = document.createElement("th");
+      newAuthor.innerHTML = authorInput.value;
+      bookListRow.appendChild(newAuthor);
+
+      const newIsbn = document.createElement("th");
+      newIsbn.innerHTML = isbnInput.value;
+      bookListRow.appendChild(newIsbn);
+
+      bookList.appendChild(bookListRow);
+    }
+})
